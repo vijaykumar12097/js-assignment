0aac14ae12a2122e30b587c4375fb1cd1ad571ac
Vijay Kumar <akshaykumar12097@gmail.com>
2021-04-20 00:10:22 +0500

    42-chapter-done

--- app.js
+++ app.js
@@ -1296,6 +1296,631 @@ result in your browser:*/
 // document.write("There are " + count + " occurrence(s) of the word 'the'");
 
 // // end of assignment21-25
+// assignment26-30 task1
+// var a=prompt("Enter any positive integer");
+// var b=Math.round(a);
+// var c=Math.ceil(a);
+// var d=Math.floor(a);
+// document.write("number: "+a+"<br>");
+// document.write("round of value: "+b+"<br>");
+// document.write("floor value: "+d+"<br>");
+// document.write("ciel value: "+c);
+
+
+// // assignment26-30 task2
+// var a=prompt("Enter any negative integer");
+// var b=Math.round(a);
+// var c=Math.ceil(a);
+// var d=Math.floor(a);
+// document.write("number: "+a+"<br>");
+// document.write("round of value: "+b+"<br>");
+// document.write("floor value: "+d+"<br>");
+// document.write("ciel value: "+c);
+
+
+// // assignment26-30 task3
+// var a=prompt("Enter any negative number");
+// var b=Math.abs(a);
+// document.write("The absolute value of "+a+" is "+b);
+
+
+// // assignment26-30 task4
+// var a=Math.random();
+// var b=Math.round(a*5)+1;
+// document.write("random dice value: "+b);
+
+
+// // assignment26-30 task5
+// var a=Math.random();
+// var b=Math.round(a*1)+1;
+// document.write(b+"<br>");
+// if(b===1){
+// document.write("random coin value: Tails");
+// }
+// else{
+// document.write("random coin value: Heads");
+// }
+
+
+// // assignment26-30 task6
+// var a=Math.random();
+// var b=Math.round(a*99)+1;
+// document.write("random number between 1 and 100: "+b);
+
+
+// // assignment26-30 task7
+// var a=prompt("Enter your wieght in kgs");
+// var b=parseFloat(a);
+// document.write("The weight of user is "+b+" kilograms");
+
+
+// // assignment26-30 task8
+// var a=Math.random();
+// var b=Math.round(a*9)+1;
+// var c=prompt("Enter a number betwwen 1 and 10");
+// if(c===b){
+//     document.write("Congartulations");
+// }
+// else{
+//     alert("Try again!");
+// }
+
+// // end of assignment26-30
+
+
+
+
+
+
+
+// // start of assignment31-34
+
+// // assignment31-34 task1
+// var a=new Date();
+// document.write(a);
+
+
+// // assignment31-34 task2
+// var monthNames = ["January", "February ", "March", "April", "May", "June", "July","August","September","October","November","December"];
+// var a= new Date();
+// var theMonth = a.getMonth();
+// var result = monthNames[theMonth];
+// alert("Current Month: "+result);
+
+
+// // assignment31-34 task3
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var now = new Date();
+// var theDay = now.getDay();
+// var nameOfToday = dayNames[theDay];
+// alert("Today is: "+nameOfToday);
+
+
+// // assignment31-34 task4
+// var dayNames = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+// var now = new Date();
+// var theDay = now.getDay();
+// var nameOfToday = dayNames[theDay];
+// if(nameOfToday===dayNames[0]||nameOfToday===dayNames[6]){
+//     alert("It’s Fun day”");
+// }
+// else{
+//     alert("Today is: "+nameOfToday);
+// }
+
+
+// // assignment31-34 task5
+// var a=new Date();
+// var b=a.getDate();
+// if(b<16){
+// document.write("First fifteen days of the month");
+// }
+// else{
+// document.write("Last days of the month");  
+// }
+
+
+// // assignment31-34 task6
+// var a=new Date();
+// document.write("Current Date: "+a+"<br>");
+// var c=a.getTime();
+// document.write("Elapsed milliseconds since january 1, 1970: "+c+"<br>");
+// var d=a.getMinutes();
+// document.write("Elapsed minutes since january 1, 1970: "+d);
+
+
+// // assignment31-34 task7
+// var a=new Date();
+// var b=a.getHours();
+// if(b<12){
+// alert("It's AM");
+// }
+// else{
+// alert("It's PM");  
+// }
+
+
+// // assignment31-34 task8
+// var a = new Date();
+// var laterDate = new Date("Dec 31, 2020");
+// document.write(laterDate);
+
+
+// // assignment31-34 task9
+// var a = new Date();
+// var b = a.getTime();
+// var c = new Date("Apr 24, 2020");
+// var d = c.getTime();
+// var e = b - d;
+// var res = e/(1000*60*60*24);
+// res = Math.floor(res);
+// document.write(res + " days have passed since 1st Ramazan, 2020");
+
+
+// // assignment31-34 task10
+// var a = new Date();
+// var b = a.getTime();
+// var c = new Date("Jan 01, 2015");
+// var d =c.getTime();
+// var e = b - d;
+// var res = e/(1000);
+// res = Math.floor(res);
+// document.write("On reference date: " + a + "<br>")
+// document.write(res + " seconds had passed since beginning of 2015");
+
+
+// // assignment31-34 task11
+// var a = new Date();
+// var b = a.getHours();
+// b = b-1
+// document.write("current date: " + a + "<br>");
+// a.setHours(b);
+// document.write("1 hour ago, it was " + a);
+
+
+// // assignment31-34 task12
+// var a = new Date();
+// var b= a.getFullYear();
+// b = b-100;
+// document.write("current date: " + a + "<br>");
+// a.setFullYear(b);
+// document.write("100 years back, it was " + a);
+
+
+
+// // assignment31-34 task13
+// var age = +prompt("Enter your age");
+// var a = new Date();
+// var b = a.getFullYear();
+// var res = b- age;
+// document.write("Your age is " + age + "<br>");
+// document.write("Your birth year is " + res);
+
+
+// // assignment31-34 task14
+// var a= "Ranjeet";
+// var b = new Date();
+// c = [ "January", "Febmliruary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+// var d = b.getMonth();
+// var e = 410;
+// var f = 16;
+// var g = e * f;
+// var h = 350;
+// var i = g + h;
+// document.write("K-Electric Bill" + "<br>" + "<br>");
+// document.write("Customer Name: " + a+ "<br>");
+// document.write("Month: " + c[d] + "<br>");
+// document.write("No of units: " + e + "<br>");
+// document.write("Charges per unit: " + f + "<br>" + "<br>");
+// document.write("Net Amount Payable (within Due Date): " + g+ "<br>");
+// document.write("Late Payment Surcharge: " + h+ "<br>");
+// document.write("Gross Amount Payable (after Due Date): " + i + "<br>");
+
+// // end of assignment31-34 
+
+
+
+
+
+
+// // start of assignment35-38
+
+
+// // assignment35-38 task1
+// function distime(){
+// var a=new Date();
+// return a;
+// }
+// document.write(distime());
+
+
+// // assignment35-38 task2
+// function add(){
+//     var a=prompt("Enter First name");
+//     var b=prompt("Enter Last name");
+//     var c=(a+" "+b);
+//     return c;
+// }
+// alert("Hello "+add());
+
+
+
+// // assignment35-38 task3
+// function add(){
+//     var a=parseFloat(prompt("Enter First number"));
+//     var b=parseFloat(prompt("Enter second number"));
+//     var c=(a+b);
+//     return c;
+// }
+// alert("Sum of two numbers is: "+add());
+
+
+// // assignment35-38 task4
+// function calcu(){
+//     var a=parseFloat(prompt("Enter First number"));
+//     var b=parseFloat(prompt("Enter second number"));
+//     var c=prompt("Enter operator which you want to perform");
+//     if(c==="+"){
+//         var d=(a+b);
+//         return d;
+//     }
+//     else if(c==="*"){
+//         var d=(a*b);
+//         return d;
+//     }
+//     else if(c==="/"){
+//         var d=(a/b);
+//         return d;
+//     }
+//     else if(c==="-"){
+//         var d=(a-b);
+//         return d;
+//     }
+//     else{
+//         return console.error();   
+//     }
+// }
+// document.write("Calculated Result : "+calcu());
+
+
+
+// // assignment35-38 task5
+// function squares(){
+//     var a=parseFloat(prompt("Enter any number"));
+//     var b=a*a;
+//     return b;
+// }
+// alert("Square of inputted number is: "+squares());
+
+         
+
+// // assignment35-38 task6
+// function fact(){
+//     var factans=1;
+//     var a=parseInt(prompt("Enter any number"));
+//     for(var i=1; i<=a; i++){
+//         factans=factans*i;
+//     }
+//     return factans;
+// }
+// alert("Factorial of inputted number is: "+fact());
+
+
+
+// // assignment35-38 task7
+// function count(){
+// var a=prompt("Enter the starting number");
+// var b=prompt("Enter the ending number");
+// var result=0;
+// for(var i=a; i<=b; i++){
+//    result +=i;
+// }
+// return(result);
+// }
+// alert("Counting from a to b is: "+count());
+
+
+// // assignment35-38 task8
+// var base=prompt("Enter base value to calculate right angle triangle");
+// var perp=prompt("Enter perpendicular value to calculate right angle triangle");
+// var hyp1;
+// var hyp2;
+// function calculateHypotenuse(){
+//   hyp1=((base*base)+(perp*perp));
+// function calculatesquare(){
+//     hyp2=Math.sqrt(hyp1);
+//   }
+//   calculatesquare();
+//   return hyp2;
+// }
+// document.write("Result: "+calculateHypotenuse());
+
+
+// // assignment35-38 task9
+// var b=4;
+// var a=prompt("Enter hieght to calculate area of triangle where width is 4");
+// var ans;
+// function triangle(){
+//   ans=b*a;
+//   return ans;
+// }
+// alert("area of triangle is "+triangle());
+
+
+// // assignment35-38 task10
+// var s=prompt("Enter any word to check whether it is palindrome or not?")
+// function isPalindrome () {
+//   var sRev = '';
+//   for (var i=s.length; i>0; i--) {
+//       sRev += s[i-1] ;
+//   }
+//   return s==sRev;
+// }
+// alert("result : "+isPalindrome());
+
+
+
+// // assignment35-38 task11
+// var str=prompt("Enter any sentence");
+// function uppercase()
+// {
+//   var array1 = str.split(' ');
+//   var newarray1 = [];   
+//   for(var x = 0; x < array1.length; x++){
+//       newarray1.push(array1[x].charAt(0).toUpperCase()+array1[x].slice(1));
+//   }
+//   return newarray1.join(' ');
+// }
+// document.write("Example string : "+str+"<br>");
+// document.write("Expected outcome : "+uppercase());
+
+
+
+// // assignment35-38 task12
+// var text=prompt("Enter any sentence");
+// function longestWord() {
+//   let wordArray = text.split(' ')
+//   let maxLength = 0
+//   let result = ''
+
+//   for (let i = 0; i < wordArray.length; i++) {
+//       if (wordArray[i].length > maxLength) {
+//           maxLength = wordArray[i].length
+//           result = wordArray[i]
+//       }
+//   }
+
+//   return result
+// }
+// document.write("Example string : "+text+"<br>");
+// document.write("Expected outcome : "+longestWord());
+
+
+
+// // assignment35-38 task13
+// function char_count(str, letter) 
+// {
+//  var letter_Count = 0;
+//  for (var position = 0; position < str.length; position++) 
+//  {
+//     if (str.charAt(position) == letter) 
+//       {
+//       letter_Count += 1;
+//       }
+//   }
+//   return letter_Count;
+// }
+// alert(char_count('JSResourceS.com', 'o'));
+
+
+
+
+// // assignment35-38 task14
+// var r=prompt("Enter radius");
+// var pie=3.14;
+// var ans1;
+// var ans2;
+// function calcCircumference(){
+//   ans1=(2*pie*r);
+//   return ans1;
+// }
+// function calcArea(){
+//   ans2=(pie*(r*r));
+//   return ans2;
+// }
+// document.write("The circumference is: "+calcCircumference()+"<br>");
+// document.write("The area is: "+calcArea());
+
+// // end of assignment35-38 
+
+
+
+
+
+
+// // Start of assignment38-42
+
+// // assignment38-42 task1
+// var a,b,c,res;
+// function power(a,b){
+//     c=Math.pow(a,b);
+//     return c;
+// }
+// a=parseFloat(window.prompt("Enter value of a"));
+// b=parseFloat(window.prompt("Enter value of b"));
+// res=power(a,b);
+// document.write("Result of a power b is :"+res);
+
+
+// // assignment38-42 task2
+// var year;
+// function Leap(year){ 
+//         if (year % 400 == 0) 
+//             return true; 
+//         if (year % 100 == 0) 
+//             return false;  
+//         if (year % 4 == 0) 
+//             return true; 
+//         return false;
+// }
+// year=parseInt(window.prompt("Enter any year to check weather it is leap year or not"));
+// document.write(Leap(year)? "Entered year is Leap Year" : "Entered year is Not a Leap Year" );
+
+
+// // assignment38-42 task3
+// var a,b,c,S,res,ans;
+// a=parseFloat(window.prompt("Enter value of a to calculate area of triangle"));
+// b=parseFloat(window.prompt("Enter value of b to calculate area of triangle"));
+// c=parseFloat(window.prompt("Enter value of c to calculate area of triangle"));
+// function area(a,b,c){
+//     if (a < 0 || b < 0 || c < 0 || (a+b <= c) || a+c <= b || b+c <= a){
+//         document.write("Not Valid Triangle");
+//     }
+//     else{
+//         ans=result(a,b,c);
+//         return ans;
+//     }
+// }
+//     function result(a,b,c){
+//         S=((a+b+c)/2);
+//         res=Math.sqrt(S*(S-a)*(S-b)*(S-c));
+//         return res;
+//     }
+// document.write("area of triangle is :"+area(a,b,c));
+
+
+
+// // assignment38-42 task4
+// var mark1=parseFloat(window.prompt("Enter marks of subject one"));
+// var mark2=parseFloat(window.prompt("Enter marks of subject two"));
+// var mark3=parseFloat(window.prompt("Enter marks of subject three"));
+// var tmarks=300;
+// var ave,perce;
+// function mainFunction(mark1,mark2,mark3){
+//     if(mark1>100||mark2>100||mark3>100){
+//         document.write("Invalid inputs");
+//     }
+//     else{
+//     var ans=("Average of obtained marks :"+avera(mark1,mark2,mark3)+"<br>"+"Percentage of obtained marks: "+perce(mark1,mark2,mark3)+"%");
+//     return ans;
+//     }
+// }
+// function avera(mark1,mark2,mark3){
+//     ave=((mark1+mark2+mark3)/3);
+//     return ave;
+// }
+// function perce(mark1,mark2,mark3){
+//     perce=(((mark1+mark2+mark3)/tmarks)*100);
+//     return perce;
+// }
+// document.write(mainFunction(mark1,mark2,mark3));
+
+
+
+// // assignment38-42 task5 incomplete
+// var a=("I am Ranjeet");
+// function pos(){
+//     var b=a.indexOf("R");
+//     return b;
+// }
+// document.write(a+"<br>"+"Position of R is :"+pos());
+
+
+
+// // assignment38-42 task6
+// var string = window.prompt("Enter any string");
+// var vowel = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"];
+
+// String.prototype.character = function name() {
+//     var i;
+//     for ( i = 0; i < vowel.length; i++) {
+//         var secondLoop = string.length;
+//         for ( j = 0; j < secondLoop; j++) {
+//             if (vowel[i] == string.charAt(j)) {
+//                 string = string.slice(0, j).concat(string.slice(j + 1, secondLoop));
+//                 j--;          
+//                 secondLoop--;  
+//             }
+
+//         }
+//     }
+// }
+// string.character();
+// document.write("Final ans after removing vowels: "+string);
+
+
+
+// // assignment38-42 task7
+// var a = "Pleases read this application and give me gratuity";
+// var count = 0;
+// let vowel= false;
+// function consecutive() {
+// for (const letter of a.toLowerCase()) {
+//   switch (letter) {
+//     case 'a':
+//     case 'e':
+//     case 'i':
+//     case 'o':
+//     case 'u':
+//       {
+//         if (vowel) {
+//           count++;
+//           vowel= false;
+//         } else {
+//           vowel= true;
+//         }
+//         break;
+//       }
+//     default:
+//       vowel= false
+//   }
+// }
+// return count
+// }
+
+// document.write(a+"<br>"+"final ans after removing  two vowels in succession :"+consecutive());
+
+
+
+// // assignment38-42 task8
+// var a=parseInt(window.prompt("Enter city distance in kilometers"));
+// function meter(){
+//     var b=a*1000;
+//     return b;
+// }
+// function feet(){
+//     var c=a*3280.84;
+//     return c;
+// }
+// function inch(){
+//     var d=a*39370.1;
+//     return d;
+// }
+// function centimeter(){
+//     var e=a*100000;
+//     return e;
+// }
+// document.write("Inputted distance in kilometers is: "+a+"km"+"<br>"+"Inputted distance in meters is: "+meter()+"m"+
+// "<br>"+"Inputted distance in feets is: "+feet()+"foot"+"<br>"+"Inputted distance in inches is: "+inch()+"inches"
+// +"<br>"+"Inputted distance in centimeters is: "+centimeter()+"cm");
+
+
+// // assignment38-42 task9 incomplete
+// var a=window.prompt("Enter the Hours for an employee worked above 40 hours : ");
+// var over_time_pay,overtime;
+// if(a>40){
+//     over_time_pay = a * 12.00;
+// }
+
+// document.write("Overtime of this employee is Rs."+over_time_pay);
+
+
+// // assignment38-42 task10
+// var Amount = parseInt(window.prompt("Please Enter Amount for Withdraw :"));
+// document.write( "Required notes of 100 is : " + parseInt((Amount / 100))+"<br>");
+// document.write( "Required notes of 50 is : " + parseInt((Amount % 100) / 50)+"<br>");
+// document.write( "Required notes of 10 is : " + parseInt((((Amount % 100) % 50) / 10))+"<br>");
+// document.write( "Amount still remaining is : " + parseInt((((Amount % 100) % 50) % 10))+"<br>");
 
 
 
